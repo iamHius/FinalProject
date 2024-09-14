a485d0186ed617a814c147271ce60b213b9b3db5
QuangCong09 <2100010440@nttu.edu.vn>
2024-09-14 16:53:32 +0700

    Connect Database

--- FinalProject/Areas/Identity/Data/ApplicationDbContext.cs
+++ FinalProject/Areas/Identity/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using FinalProject.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +11,12 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
         : base(options)
     {
     }
+    public DbSet<RoomType> RoomTypes { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<Size> Sizes { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<Capacity> Capacitys { get; set; }
+    public DbSet<Invoice> Invoices { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -17,5 +24,14 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
+
+        foreach (var entityType in builder.Model.GetEntityTypes())
+        {
+            var tableName = entityType.GetTableName();
+            if (tableName.StartsWith("AspNet"))
+            {
+                entityType.SetTableName(tableName.Substring(6));
+            }
+        }
     }
 }
